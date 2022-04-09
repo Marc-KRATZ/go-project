4d4f76338a78d1880db30d8b4697173919a6246f
Marc-KRATZ <kratzmarc@gmail.com>
2022-04-09 14:27:02 +0200

    fisrt commit

---
+++ main.go
@@ -0,0 +1,54 @@
+package main
+
+import (
+	"go_project/src/controller"
+	"go_project/src/middleware"
+	"go_project/src/service"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+func setupRouter() *gin.Engine {
+
+	var loginService service.LoginService = service.NewLoginService()
+	var jwtService service.JWTService = service.NewJWTService()
+	var loginController controller.LoginController = controller.NewLoginController(loginService, jwtService)
+	// Disable Console Color
+	// gin.DisableConsoleColor()
+	r := gin.Default()
+
+	r.POST("/login", func(ctx *gin.Context) {
+		token := loginController.Login(ctx)
+		if token != "" {
+			ctx.JSON(http.StatusOK, gin.H{
+				"token": token,
+			})
+		} else {
+			ctx.JSON(http.StatusUnauthorized, nil)
+		}
+	})
+
+	// Ping test
+	r.GET("/health", func(c *gin.Context) {
+		c.JSON(http.StatusOK, gin.H{"Status": "OK"})
+
+	})
+
+	apiRoutes := r.Group("/api", middleware.AuthorizeJWT())
+	{
+		apiRoutes.GET("/hello", func(ctx *gin.Context) {
+			ctx.JSON(200, gin.H{
+				"message": "pong",
+			})
+		})
+	}
+
+	return r
+}
+
+func main() {
+	r := setupRouter()
+	// Listen and Server in 0.0.0.0:8080
+	r.Run(":8080")
+}
---
+++ src/middleware/JWTMiddleware.go
@@ -0,0 +1,30 @@
+package middleware
+
+import (
+	"fmt"
+	"go_project/src/service"
+	"log"
+	"net/http"
+
+	"github.com/dgrijalva/jwt-go"
+	"github.com/gin-gonic/gin"
+)
+
+func AuthorizeJWT() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		const BEARER_SCHEMA = "Bearer "
+		authHeader := c.GetHeader("Authorization")
+		tokenString := authHeader[len(BEARER_SCHEMA):]
+		log.Println("tokenString", tokenString)
+		token, err := service.NewJWTService().ValidateToken(tokenString)
+		log.Println("token", token)
+		if token.Valid {
+			claims := token.Claims.(jwt.MapClaims)
+			fmt.Println(claims)
+		} else {
+			fmt.Println(err)
+			c.AbortWithStatus(http.StatusUnauthorized)
+		}
+
+	}
+}
